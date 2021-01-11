c5fd8af23c616d899d1ed0933e811e5b7a6d9b95
T-Stewart <70151951+T-Stewart@users.noreply.github.com>
2021-01-11 21:49:39 +0000

    mobile changes

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -19,7 +19,7 @@ export default class Experience extends React.Component{
                 <a className="link" target="blank" href="https://github.com/T-Stewart/JS-Card-Game">Card Game</a>
                 <p>A playing card game</p>
                 <br/>
-                <a className="link" target="blank" href="https://competent-hugle-6b7204.netlify.app">Semi-Toned</a>
+                <a className="link" target="blank" href="https://semi-tonedpodcast.netlify.app/">Semi-Toned</a>
                 <p>A website for my friend's and my podcast</p>
             </div>
         )
